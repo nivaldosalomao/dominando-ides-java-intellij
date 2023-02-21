ba535217df6a889d5ab28d25c5ee7abae355bb5d
Nivaldo <nivaldosalomaodepontes@gmail.com>
2023-02-21 14:37:08 -0300

    First Commit

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+        Gato grafield = new Gato("nome", "vermelho", 35);
+        System.out.println(grafield);
+
+
+    }
+
+}
